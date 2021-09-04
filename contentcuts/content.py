8c23600fae8815236f8305babbad9777c77eb031
Ein_Green <mbananola2002@gmail.com>
2021-09-04 14:01:48 -0600

    major changes to content.py, trying to fix bugs. Under testing

--- contentcuts/content.py
+++ contentcuts/content.py
@@ -30,64 +30,62 @@ def show_content():
 
 def create_content():
     try: 
-        token = str(request.json["loginToken"])
+        token = str(request.json["loginToken"]) #* Required
+        user_id = request.json["posterId"] #* Required
+        read_type = str(request.json["readType"]) #* Required
+        title = str(request.json["title"]) #* Required
+        author = str(request.json["author"]) #* Required
+        cover = str(request.json["cover"]) #* Required
+        artist = str(request.json["artist"]) #* Required
+        status = request.json.get("status")
+        tags = request.json.get("tags")
     except IndexError:
         return Response("Data invalid", mimetype="text/plain", status=404)
+    except KeyError:
+        return Response("Dude, some of this stuff is required, you have to put it in the thing", mimetype="text/plain", status=401)
     except:
         traceback.print_exc()
-        print("No Token given")
-        return Response("Data Error", mimetype="text/plain", status=400)
+        return Response("A thing went wrong", mimetype="text/plain", status=400)
     
     new_content = ""
     is_author = checks.check_author(token)
     
     if(is_author == None or "" or False):
         return Response("User not authorized to post content", mimetype="text/plain", status=401)
-    elif(is_author != None or "" or False):
-        try: 
-            user_id = request.json["posterId"]
-            read_type = str(request.json["readType"])
-            title = str(request.json["title"])
-            author = str(request.json["author"])
-            cover = str(request.json["cover"])
-            artist = str(request.json["artist"])
-            status = request.json.get("status")
-            tags = request.json.get("tags")
-        except:
-            traceback.print_exc()
-            print("Can't post, thing went wrong")
-            return Response("Data Error, request invalid", mimetype="text/plain", status=400)
+    elif(is_author != None and len(is_author) != 0):
         if((tags == None or "") and (status == None or "")):
             new_content = dbshorts.run_insertion("insert into content (read_type, title, author, cover, artist, poster_id) values (?,?,?,?,?,?)",
-                                                    [read_type, title, author, cover, artist, user_id])
+                                                 [read_type, title, author, cover, artist, user_id])
         elif(status == None or status == ""):
             new_content = dbshorts.run_insertion("insert into content (read_type, title, author, cover, artist, tags, poster_id) values (?,?,?,?,?,?,?)",
-                                                    [read_type, title, author, cover, artist, tags, user_id])
+                                                 [read_type, title, author, cover, artist, tags, user_id])
         elif(tags == None or tags == ""):
             new_content = dbshorts.run_insertion("insert into content (read_type, title, author, cover, artist, status, poster_id) values (?,?,?,?,?,?,?)",
-                                                    [read_type, title, author, cover, artist, status, user_id])
+                                                 [read_type, title, author, cover, artist, status, user_id])
         else:
             new_content = dbshorts.run_insertion("insert into content (read_type, title, author, cover, artist, status, tags, poster_id) values (?,?,?,?,?,?,?,?)",
-                                                    [read_type, title, author, cover, artist, status, tags, user_id])
+                                                 [read_type, title, author, cover, artist, status, tags, user_id])
     
-    if(new_content != None or new_content != ""):
-        content_info = dbshorts.run_selection("select c.id, c.read_type, c.title, c.author, c.cover, c.artist, c.status, c.tags, c.date_posted, c.poster_id from content c", [])
-        content_dictionary = {"contentId": content_info[0][0],
-                              "readType": content_info[0][1],
-                              "title": content_info[0][2],
-                              "author": content_info[0][3],
-                              "cover": content_info[0][4],
-                              "artist": content_info[0][5],
-                              "status": content_info[0][6],
-                              "tags": content_info[0][7],
-                              "datePosted": content_info[0][8],
-                              "posterId": content_info[0][9]}
-        content_json = json.dumps(content_dictionary, default=str)
-        print("SUCCESS [probably]")
-        print(content_dictionary)
-        return Response(content_json, mimetype="application/json", status=201)
+        if(new_content != None or new_content != ""):
+            content_info = dbshorts.run_selection("select c.id, c.read_type, c.title, c.author, c.cover, c.artist, c.status, c.tags, c.date_posted, c.poster_id from content c", [])
+            content_dictionary = {"contentId": content_info[0][0],
+                                "readType": content_info[0][1],
+                                "title": content_info[0][2],
+                                "author": content_info[0][3],
+                                "cover": content_info[0][4],
+                                "artist": content_info[0][5],
+                                "status": content_info[0][6],
+                                "tags": content_info[0][7],
+                                "datePosted": content_info[0][8],
+                                "posterId": content_info[0][9]}
+            content_json = json.dumps(content_dictionary, default=str)
+            print("SUCCESS [probably]")
+            print(content_dictionary)
+            return Response(content_json, mimetype="application/json", status=201)
+        else:
+            return Response("Database Error, something went wrong on our end, sorry mate", mimetype="text/plain", status=500)
     else:
-        return Response("Database Error, something went wrong on our end, sorry mate", mimetype="text/plain", status=500)
+        return Response("Error on posting content", mimetype="text/plain", status=401)
 
 def delete_content():
     try:
